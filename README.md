dbe0139409bec2e1bb3f603b4ca0c1ec33d2d96f
johnmelodev <joaohenrique1231500@gmail.com>
2024-03-20 14:38:40 -0300

    1- SQL configured

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ assim criar um agendamento para que rode todo dia as 6 da manha
 foi criado um site ficticion para puxar funcionalidades online chamado 
 https://site1produto.netlify.app
 porque caso fizesse em sites reais de preco, iriam mudar constantemente transformando as funcionalidades obsoletas
+
+como esse foi um projeto feito para um cliente privado, para exportar as funcionalidades de scrapy e front end e back end com django exportei para o github com um site ficticio
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException
 import psycopg2
 from datetime import datetime, timezone
 from selenium import webdriver
@@ -7,7 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import *
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support import expected_conditions as condicao_esperada
+from selenium.webdriver.support import expected_conditions
 import schedule
 from time import sleep
 
@@ -37,8 +38,81 @@ def new_product(sql, connection, name, price, site, date_quotation, image_link):
     else:
         print("Data already registered previously")
 
+    connection.commit()
 
-new_product(sql, connection, 'Iphone 14', 13000.50,
-            'apple.com/iphone 14', datetime.now(), 'https://via.placeholder.com/150')
+# Webscraping with Selenium
 
-connection.commit()
+
+def iniciar_driver():
+    chrome_options = Options()
+
+    arguments = ['--lang=en-us', '--window-size=1920,1080',
+                 '--incognito']
+    # '--headless'
+
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    chrome_options.add_experimental_option('prefs', {
+        'download.prompt_for_download': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'profile.default_content_setting_values.automatic_downloads': 1
+    })
+
+    driver = webdriver.Chrome(service=ChromeService(
+        ChromeDriverManager().install()), options=chrome_options)
+    wait = WebDriverWait(
+        driver,
+        10,
+        poll_frequency=1,
+        ignored_exceptions=[
+            NoSuchElementException,
+            ElementNotVisibleException,
+            ElementNotSelectableException,
+        ]
+    )
+    return driver, wait
+
+
+def scrapy_website1():
+    driver, wait = iniciar_driver()
+    driver.get('https://site1produto.netlify.app')
+    # Xpath = //tag[@attribute='value']
+    # name of the product
+    names = wait.until(expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, "//div[@class='detail-box']/a")))
+    # get price
+    prices = wait.until(expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, "//h6[@class='price_heading']")))
+    # get site
+    site = driver.current_url
+    # image link
+    image_link = wait.until(expected_conditions.visibility_of_all_elements_located(
+        (By.XPATH, "//div[@class='img-box']/img")))
+    iphone_name = names[0].text
+    gopro_name = names[1].text
+
+    iphone_price = prices[0].text.split(' ')[1]
+    gopro_price = prices[1].text.split(' ')[1]
+
+    iphone_image = image_link[0].get_attribute('src')
+    gopro_image = image_link[1].get_attribute('src')
+
+    # date quotation
+    new_product(sql, connection, iphone_name, iphone_price,
+                site, datetime.now(), iphone_image)
+    new_product(sql, connection, gopro_name, gopro_price,
+                site, datetime.now(), gopro_image)
+
+
+def scrapy_website2():
+    driver, wait = iniciar_driver()
+    driver.get('https://site2produto.netlify.app')
+
+
+def scrapy_website3():
+    driver, wait = iniciar_driver()
+    driver.get('https://site3produto.netlify.app')
+
+
+scrapy_website1()
